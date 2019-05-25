db3ba649d64783686c76ad295f6e4d2b03f2885d
kal J <kharljay15@gmail.com>
2019-05-25 16:30:20 +0300

    Added login Input Validation function

---
+++ validation/login.js
@@ -0,0 +1,28 @@
+//pulling in dependencies
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+module.exports = function validateLoginInput(data){
+    let errors = {};
+    
+    //Convert empty fields to strings so we can use validator functions
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
+
+    //Email checks
+    if(Validator.isEmpty(data.email)){
+        errors.email = "Email is required";
+    }else if(!Validator.isEmail(data.email)){
+        errors.email = "Invalid Email";
+    }
+
+    //Password checks
+    if(Validator.isEmpty(data.password)){
+        errors.password = "Password is required";
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
