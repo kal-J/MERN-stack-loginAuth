d5c21911eb2a1d978acefc0db7d88b4bf777d7b4
kal J <kharljay15@gmail.com>
2019-05-25 21:19:45 +0300

    Editted keys and opts constants

--- config/passport.js
+++ config/passport.js
@@ -2,11 +2,11 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
-const keys = require('./keys');
+const keys = require('../config/keys');
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretorKey = keys.secretorKey;
+opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
     passport.use(
