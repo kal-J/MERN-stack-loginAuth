1ecc471ec03e0e5784f939345559e16e45c43d30
kal J <kharljay15@gmail.com>
2019-06-12 14:04:21 +0300

    .

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI : "mongodb://127.0.0.1:27017/users",
+    mongoURI : "mongodb+srv://kal:kalmernstack@mernstackauth-mefzv.gcp.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "secret"
 }
