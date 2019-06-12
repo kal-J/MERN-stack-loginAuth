220078fffbab775bbab42585c51cf01a66a06c5a
kal J <kharljay15@gmail.com>
2019-06-12 13:07:35 +0300

    prepared for deployement to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cors = require('cors');
+const path = require('path');
 
 const users = require('./routes/api/users');
 
@@ -25,7 +26,14 @@ mongoose.connect(db,{useNewUrlParser:true})
         require('./config/passport') (passport);
 
         app.use('/api/users', users);
-
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+        //set static folder
+        app.use(express.static('client/build'));
+        app.get('*',(req,res) => {
+                res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+        });
+}
 //set up PORT
 const PORT = process.env.PORT || 5000;
 
